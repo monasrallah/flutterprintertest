123837d4cad5f8f279dbc04967b2a352c8edeabd
monasrallah <55625927+monasrallah@users.noreply.github.com>
2024-07-23 14:23:29 +0300

    e

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,15 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<UsbDevice> devices = [];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,38 +23,55 @@ class MyApp extends StatelessWidget {
           title: const Text('USB Printer Example'),
         ),
         body: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              List<UsbDevice> devices = await UsbSerial.listDevices();
-              print(devices);
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  List<UsbDevice> devices = await UsbSerial.listDevices();
+                  setState(() {
+                    this.devices = devices;
+                  });
+                  print(devices);
 
-              UsbPort port;
-              if (devices.isEmpty) {
-                return;
-              }
-              port = (await devices[0].create())!;
+                  UsbPort port;
+                  if (devices.isEmpty) {
+                    return;
+                  }
+                  port = (await devices[0].create())!;
 
-              bool openResult = await port.open();
-              if (!openResult) {
-                print("Failed to open");
-                return;
-              }
+                  bool openResult = await port.open();
+                  if (!openResult) {
+                    print("Failed to open");
+                    return;
+                  }
 
-              await port.setDTR(true);
-              await port.setRTS(true);
+                  await port.setDTR(true);
+                  await port.setRTS(true);
 
-              port.setPortParameters(001, UsbPort.DATABITS_8,
-                  UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+                  port.setPortParameters(001, UsbPort.DATABITS_8,
+                      UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
 
-              // print first result and close port.
-              port.inputStream?.listen((Uint8List event) {
-                print(event);
-                port.close();
-              });
+                  // print first result and close port.
+                  port.inputStream?.listen((Uint8List event) {
+                    print(event);
+                    port.close();
+                  });
 
-              await port.write(Uint8List.fromList([0x001, 0x003]));
-            },
-            child: const Text('Print'),
+                  await port.write(Uint8List.fromList([0x001, 0x003]));
+                },
+                child: const Text('Print'),
+              ),
+              if (devices.isEmpty) const Text("empye"),
+              if (devices.isNotEmpty)
+                ...List.generate(
+                  devices.length,
+                  (index) {
+                    return Text(devices[index].deviceName);
+                  },
+                )
+            ],
           ),
         ),
       ),
